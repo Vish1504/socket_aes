6d8135768dd6d9ce86c3651e164b8b24ad94f4ee
Vishnu Nair <visair1504@gmail.com>
2023-03-04 22:15:13 +0530

    Added RSA

--- server.js
+++ server.js
@@ -7,14 +7,8 @@ const port = process.env.PORT || 8080;
 const crypto = require('crypto');
 
 // Generate a 32-byte AES key
-const encryptionKey = 'ThisIsA32ByteKeyForAES1234567890';
+const encryptionKey = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
-
-// Create a Cipher object with the AES algorithm and CBC mode
-const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
-
-// Create a Decipher object with the AES algorithm and CBC mode
-const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
 
 // Handle incoming socket connections
 io.on('connection', (socket) => {
@@ -34,11 +28,63 @@ io.on('connection', (socket) => {
 
     // Handle incoming chat messages
     socket.on('chat message', (message) => {
+
+        // Create a Cipher object with the AES algorithm and CBC mode
+        const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
+
         // Encrypt the message using the AES key
         let aes_encrypted = cipher.update(message, 'utf8', 'base64');
         aes_encrypted += cipher.final('base64');
 
         console.log('encrypted message: ' + aes_encrypted);
+
+
+        //RSA stuff
+        const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+            // The standard secure default length for RSA keys is 2048 bits
+            modulusLength: 2048,
+        });
+
+        const pubkey = publicKey.export({
+            type: "spki",
+            format: "pem",
+        });
+
+        // console.log("pubkey : ", pubkey);
+
+        const privkey = privateKey.export({
+            type: "pkcs8",
+            format: "pem",
+        });
+
+        // console.log("privkey : ", privkey);
+
+        const RSAmessage = encryptionKey;
+        const encryptedData = crypto.publicEncrypt(
+            {
+                key: publicKey,
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: "sha256",
+            },
+            // We convert the data string to a buffer using `Buffer.from`
+            Buffer.from(RSAmessage)
+        );
+
+        console.log("\nRSA Encrypted data:", encryptedData.toString("base64"));
+
+        const decryptedData = crypto.privateDecrypt(
+            {
+                key: privateKey,
+                padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+                oaepHash: "sha256",
+            },
+            encryptedData
+        );
+
+        console.log("\nRSA Decrypted data:", decryptedData.toString());
+
+        // Create a Decipher object with the AES algorithm and CBC mode
+        const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), iv);
 
         // Decrypt the message using the AES key
         let aes_decrypted = decipher.update(aes_encrypted, 'base64', 'utf8');
